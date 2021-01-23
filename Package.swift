ab5b34142ad717a6fa8e2686a5a2d8d5ef922cd7
Meir Radnovich <meir@radnovich.com>
2021-01-24 00:00:27 +0200

    Exclude Info.plist to resolve build warnings

--- Package.swift
+++ Package.swift
@@ -21,9 +21,16 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PhoneNumberFormatter",
-            dependencies: []),
+            dependencies: [],
+            exclude: [
+                "Info.plist"
+            ]
+        ),
         .testTarget(
             name: "PhoneNumberFormatterTests",
-            dependencies: ["PhoneNumberFormatter"]),
+            dependencies: ["PhoneNumberFormatter"],
+            exclude: [
+                "Info.plist"
+            ]),
     ]
 )
